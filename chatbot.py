a00196f4cc49a0cd54aceea873585912ba4713c8
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-11-18 22:11:44 -0600

    adding code

---
+++ chatbot.py
@@ -0,0 +1,47 @@
+from openai import OpenAI
+import gradio as gr
+
+openai = OpenAI(
+   api_key= "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"
+ )
+
+messages = [
+    {"role":"system", "content": "This is a chatbot that only answer questions related to Plants. For questions not related to Plants, reply with Sorry, I do not know."},
+    {"role":"user","content": "What is a Plant?"},
+    {"role":"assistant","content": "It is a living organism of the kind exemplified by trees, shrubs, herbs, grasses, ferns, and mosses, typically growing in a permanent site, absorbing water and inorganic substances through its roots, and synthesizing nutrients in its leaves by photosynthesis using the green pigment"}
+]
+
+def generate_response(prompt):
+    if prompt == "Hello":
+        reply = "Hello, I'm Tom! How can I help you?"
+    
+    else:
+        messages.append({"role": "user", "content": prompt})
+        chat = openai.chat.completions.create(
+            model="gpt-3.5-turbo", max_tokens = 20, messages=messages
+        )
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+
+   
+    return reply
+
+
+def my_chatbot(input, history):
+    history = history or []
+    my_history = list(sum(history, ()))
+    my_history.append(input)
+    my_input = ' '.join(my_history)
+    output = generate_response(my_input)
+    history.append((input, output))
+    return history, history
+
+with gr.Blocks() as demo:
+    gr.Markdown("""<h1><center>Chatbot universe.ai</center></h1>""")
+    chatbot = gr.Chatbot()
+    state = gr.State()
+    text = gr.Textbox(placeholder="Explore the wonders of the plants! Ask me anything", key="Enter")  # Adjust width
+    submit = gr.Button("SEND")
+    submit.click(my_chatbot, inputs=[text, state], outputs=[chatbot, state])
+
+demo.launch(share = True)
---
+++ utils.py
@@ -0,0 +1,13 @@
+palabras_clave_universo = [
+    "universo", "cosmos","cosmo", "espacio", "astronomía", "galaxia", "estrella",
+    "planeta", "nebulosa", "agujero negro", "big bang", "telescopio",
+    "constelación", "astrofísica", "gravedad", "vía láctea", "expansión cósmica",
+    "planetario", "órbita", "sistemas solares", "exploración espacial",
+    "ciencia espacial", "astrobiología", "materia oscura", "energía oscura",
+    "rayos cósmicos", "radiación cósmica de fondo", "supernova", "cuásar",
+    "exoplaneta", "observatorio espacial", "magnetosfera", "satélite natural",
+    "teoría de la relatividad", "agua en el espacio", "astronave",
+    "órbita elíptica", "evento astronómico", "astrónomo", "centella",
+    "sistema estelar", "anomalía gravitacional", "cohete espacial",
+    "sonda espacial", "geocentrismo", "heliocentrismo", "pulsar", "magnetar",
+    "cinturón de asteroides", "meteorito", "cometa","meteoritos", "cometas"]
